d92b1bbf0f768b30b0b7d6fd05c1eb6ad8499ccb
ljhx2 <ljhx2@daum.net>
2022-07-19 01:17:11 +0900

    Add CursorController

---
+++ Assets/Scripts/Controller/CursorController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    private enum CursorType
+    {
+        None,
+        Attack,
+        Hand,
+    }
+
+    Texture2D _attackIcon;
+    Texture2D _handIcon;
+
+    CursorType _cursorType = CursorType.None;
+
+    int _clickMask = (1 << (int)Define.Layer.Ground) | (1 << (int)Define.Layer.Monster);
+
+    void Start()
+    {
+        _attackIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Attack");
+        _handIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Hand");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.red, 1f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f, _clickMask))
+        {
+            if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+            {
+                if (_cursorType != CursorType.Attack)
+                {
+                    Cursor.SetCursor(_attackIcon, new Vector2(_attackIcon.width / 5f, 0f), CursorMode.Auto);
+                    _cursorType = CursorType.Attack;
+                }
+            }
+            else
+            {
+                if (_cursorType != CursorType.Hand)
+                {
+                    Cursor.SetCursor(_handIcon, new Vector2(_handIcon.width / 3f, 0f), CursorMode.Auto);
+                    _cursorType = CursorType.Hand;
+                }
+
+            }
+        }
+    }
+}
